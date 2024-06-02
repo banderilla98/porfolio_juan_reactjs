4bf69d04aebaf1429b61568f61d631074c317814
Juan Zahid Angeles Sarabia <zahidangeles11@gmail.com>
2024-06-02 13:38:29 -0700

    nothing important here for now

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,10 +1,27 @@
 import styes from "./HeroStyles.module.css";
-import heroImage from '../../assets/heroImage.png'
+import heroImage from "../../assets/heroImage.png";
+import themeIcon from "../../assets/sun.svg";
 
 function Hero() {
-  return <section id="hero">
-    <div><img src={heroImage} alt="" /></div>
-  </section>;
+  return (
+    <section id="hero" className={styles.container}>
+      <div className="styles.colorModeContainer">
+        <img
+          className={styles.hero}
+          src={heroImage}
+          alt="Profile Picture of Juan Zahid Angeles Sarabia Portfolio"
+        />
+        <img
+          className={styles.ColorMode}
+          src={themeIcon}
+          alt="Color mode icon"
+        />
+      </div>
+      <div className={styles.info}>
+        <h1>Juan <hr />Zahid</h1>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
