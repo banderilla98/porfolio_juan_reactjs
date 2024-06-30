bcaccef10373f0e70e2b5f05032a065f624fe557
Banderilla98 <zahidangeles11@gmail.com>
2024-06-30 14:13:36 -0700

    agregando funcion con un handle click para testear clicks y mostrarlos en consola

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -1,8 +1,13 @@
 import './TicTacToe.css'
 
 
-function Square(){
-  return (<button className='square'>1</button>);
+function Square({value}){
+
+  function clickAqui(){
+    console.log('clicked aqui');
+  }
+
+  return (<button className='square' onClick={clickAqui}>{value}</button>);
 }
 
 
@@ -10,21 +15,21 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value='1'/>
+        <Square value='2'/>
+        <Square value='3'/>
       </div>
 
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value='4'/>
+        <Square value='5'/>
+        <Square value='6'/>
       </div>
 
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value='7'/>
+        <Square value='8'/>
+        <Square value='9'/>
       </div>
     </>
   );
