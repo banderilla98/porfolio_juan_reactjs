11263564a01f799806c5aeaac7b2172c9e4ba1a3
Banderilla98 <zahidangeles11@gmail.com>
2024-07-20 12:29:18 -0700

    haciendo mas pruebas de aprendizaje

--- src/components/TicTacToe/Game.jsx
+++ src/components/TicTacToe/Game.jsx
@@ -7,7 +7,7 @@ class ControlledInput extends React.Component {
       input: ''
     };
     // Change code below this line
-    
+
     this.handleChange = this.handleChange.bind(this);
 
     // Change code above this line
@@ -15,10 +15,10 @@ class ControlledInput extends React.Component {
 
   // Change code below this line
 
-  handleChange(event) {
+  handleChange(event){
     this.setState({
       input: event.target.value
-    });
+    })
   }
 
   // Change code above this line
@@ -26,7 +26,9 @@ class ControlledInput extends React.Component {
     return (
       <div>
         { /* Change code below this line */}
+        
         <input value={this.state.input} onChange={this.handleChange} />
+
         { /* Change code above this line */}
         <h1>Controlled Input:</h1>
         <p>{this.state.input}</p>
@@ -35,12 +37,52 @@ class ControlledInput extends React.Component {
   }
 }
 
+
+
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      submit: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+    this.setState({
+      submit: this.state.input
+    });
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            value={this.state.input}
+            onChange={this.handleChange} />
+          <button type='submit'>Submit!</button>
+        </form>
+        <h1>{this.state.submit}</h1>
+      </div>
+    );
+  }
+};
+
 const JSX = () => {
   return (
     <div>
       <h1>hola</h1>
       {/* to print this on website page front */}
       <ControlledInput /> 
+
+      <MyForm/>
     </div>
   );
 };
