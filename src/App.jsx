1b8ea19659765eed68a946470317efd128b8487c
Banderilla98 <zahidangeles11@gmail.com>
2024-07-23 12:43:40 -0700

    sigo intentandolo...

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import Header from "./components/Header/Header.jsx";
 import Hero from "./components/Hero/Hero.jsx";
 import CoreConceptsSection from "./components/CoreConceptsSection/CoreConceptsSection.jsx";
 import ExamplesSection from "./components/ExamplesSection/ExamplesSection.jsx";
-import Game from './components/TicTacToe/Game.jsx'
-import { TodoList } from "./components/TodoList/TodoList.jsx";
+import Game from "./components/TicTacToe/Game.jsx";
+
 
 function App() {
   return (
@@ -15,12 +15,7 @@ function App() {
       <CoreConceptsSection />
       <ExamplesSection />
 
-      <Game/>
-
-      <TodoList/>
-
-
-
+      <Game />
 
       
     </>
